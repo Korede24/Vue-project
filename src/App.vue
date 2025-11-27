68e2c459d47f679fc157f538ef84025b351b97fb
korede24 <ibrahimmuzammil2020@gmail.com>
2025-11-27 12:39:02 +0100

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
-<template lang="">
-  <nav>
+<template >
+    <Navbar :isAuthenticated="true"/>
+  <!-- <nav>
       <li><router-link to="/">Home</router-link></li>
       <li><router-link to="/about">About</router-link></li>
       <li><router-link to="/composition">Composition</router-link></li>
-  </nav>
+  </nav> -->
+
+  
+
+<h1 class="text-yellow-400 text-3xl">Welcome to Vue Router Tutorial</h1>
+
+
+
   <router-view />
 </template>
 <script>
+// export default {
+//     data() {
+//         return {
+//             count: 0
+//         }
+//     },
+// }
+import Navbar from './components/Navbar.vue';
 export default {
-    data() {
-        return {
-            count: 0
-        }
-    },
-}
+components: {Navbar},
+};
 </script>
 <style lang="">
-    
+
 </style>
